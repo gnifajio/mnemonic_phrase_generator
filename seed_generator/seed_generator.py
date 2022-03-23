131508e47f1af14530a906c1ce6e6a08f23af181
gnifajio <gnifajio@gmail.com>
2022-03-23 14:15:23 +0300

    Обновлен файл requirements.txt

--- seed_generator/seed_generator.py
+++ seed_generator/seed_generator.py
@@ -15,14 +15,14 @@ class Mnemonic:
     def generate(self):
         entropy = secrets.randbits(self.generated_bit_count)
         entropy = bin(entropy)[2:].zfill(self.generated_bit_count)
-        entropy_hash = self.get_hash(entropy)
-        indices = self.pick_words(entropy, entropy_hash)
+        entropy_hash = self._get_hash(entropy)
+        indices = self._pick_words(entropy, entropy_hash)
         words = [self.bip39wordlist[indices[i]] for i in range(0, len(indices))]
         word_string = ' '.join(words)
         return word_string
 
 
-    def pick_words(self, entropy, entropy_hash):
+    def _pick_words(self, entropy, entropy_hash):
         generated_bit_count = len(entropy)
         generated_char_count = generated_bit_count // 4
         entropy_hex = '0x{0:0{1}x}'.format(int(entropy, 2), generated_char_count)
@@ -37,7 +37,7 @@ class Mnemonic:
         return indices
 
 
-    def get_hash(self, entropy):
+    def _get_hash(self, entropy):
         generated_bit_count = len(entropy)
         generated_char_count = generated_bit_count // 4
         entropy_hex = '0x{0:0{1}x}'.format(int(entropy, 2), generated_char_count)
